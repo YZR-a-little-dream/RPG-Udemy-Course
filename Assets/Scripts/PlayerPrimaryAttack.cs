63056b1ae1077ebd4ad72b4da456d270e5e0c227
yzr <958569203@qq.com>
2024-10-03 07:20:46 +0800

    Combo Attacks

--- Assets/Scripts/PlayerPrimaryAttack.cs
+++ Assets/Scripts/PlayerPrimaryAttack.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayerPrimaryAttack : PlayerState
 {
+    private int comboCounter;                   //攻击组合记数
+    private float lastTimerAttacked;            //最后一次攻击的时间
+    private float comboWidow = 2;               //连击经过多长时间重置，默认值设为2
     public PlayerPrimaryAttack(Player _player, PlayerStateMachine _playerStateMachine, string _animaBoolName) : base(_player, _playerStateMachine, _animaBoolName)
     {
+
     }
 
     public override void Enter()
     {
         base.Enter();
+
+        if(comboCounter >2 || Time.time > lastTimerAttacked + comboWidow)
+        {
+            comboCounter = 0;
+        }
+
+        player.anim.SetInteger("ComboCounter", comboCounter);
     }
 
     public override void Exit()
     {
         base.Exit();
+
+        comboCounter++;
+        lastTimerAttacked = Time.time;
     }
 
     public override void Update()
